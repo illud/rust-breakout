7ecae1b5ea321165f45833314fefe12a1cce321f
saturnavt <saturnavt@gmail.com>
2022-07-15 21:18:29 -0500

    enemy blocks

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use macroquad::prelude::*;
 
 const PLAYER_SIZE: Vec2 = const_vec2!([150f32, 40f32]);
 const PLAYER_SPEED: f32 = 700f32;
+const BLOCK_SIZE: Vec2 = const_vec2!([150f32, 40f32]);
 
 struct Player {
     rect: Rect,
@@ -44,14 +45,47 @@ impl Player {
     }
 }
 
+struct Block {
+    rect: Rect,
+}
+
+impl Block {
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y),
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKGRAY);
+    }
+}
+
 #[macroquad::main("Rust Breakout")]
 async fn main() {
     let mut player = Player::new();
+    let mut blocks = Vec::new();
+    let (width, height) = (6, 6);
+    let padding = 5f32;
+    let total_block_size = BLOCK_SIZE + vec2(padding, padding);
+    let board_start_pos = vec2(
+        (screen_width() - (BLOCK_SIZE.x * width as f32)) * 0.5f32,
+        50f32,
+    );
+
+    for i in 0..width * height {
+        let block_x = (i % width) as f32 * total_block_size.x;
+        let block_y = (i / width) as f32 * total_block_size.y;
+        blocks.push(Block::new(board_start_pos + vec2(block_x, block_y)));
+    }
 
     loop {
         player.update(get_frame_time());
         clear_background(WHITE);
         player.draw();
+        for block in blocks.iter() {
+            block.draw();
+        }
         next_frame().await
     }
 }
